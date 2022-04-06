7de75ded00376c84a4e45bfd22c7f2c23b433a4b
HenrikVL <henrik.viken.lied@gmail.com>
2022-04-06 10:52:31 +0200

    finish 3.1 and begin on 3.2
    Co-authored-by: Hallvard Jensen <EwanMe@users.noreply.github.com>

--- flush.c
+++ flush.c
@@ -4,35 +4,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <errno.h>
+
+int exit_flag = 0;
 
 // Write prompt to screen and return input
 void prompt(char *input) {
     size_t path_size = 100;
-    char* path = malloc(sizeof(char) * path_size);
-    getcwd(path, path_size);
-    printf("%s: ", path);
-    free(path);
+    char* path = malloc(sizeof(char) * path_size); // Allocate memory for path
+    getcwd(path, path_size); // Get working directory
+    printf("%s: ", path); // Print working directory
+    free(path); // Free memory
     
-    // TODO: Fix newline at end of input.
+    // Takes input from command line
     char buffer[1024];
-    if (buffer == fgets(buffer, sizeof(buffer), stdin)) {
-        int ret = sscanf(buffer, "%s", input);
-
-        if (ret == 1) {
-            printf("buffer: %s\n input: %s\n", buffer, input);
-        }
+    
+    if (buffer == fgets(buffer, sizeof(buffer), stdin)) { 
+        buffer[strcspn(buffer, "\r\n")] = 0; // Remove newline from input
+        strcpy(input, buffer);
+    }
+    // Exits if fgets fails
+    else if (strcmp(buffer, "\0") == 0) {
+        // TODO: 3.1 NOT COMPLETED: Check for EOF instead
+        exit_flag = 1;
+        printf("EOF\n");
     }
 }
 
 // Parse input and puts args in array separated by space or tab
-void parse(char **args, char *input) {
+void parse_arguments(char **args, char *input) {
     int i = 0;
     char *token[256];
-    token[i] = strtok(input, " \t");
-    while (token[i] != NULL) {
-        args[i] = token[i];
+    
+    token[i] = strtok(input, " \t"); // Split input by space or tab
+    while (token[i] != NULL) { 
+        args[i] = token[i]; // Put token in args array
         i++;
-        token[i] = strtok(NULL, " \t");
+        token[i] = strtok(NULL, " \t"); 
     }
 }
 
@@ -41,32 +49,43 @@ int main() {
         char *input = malloc(sizeof(char) * 1024);
         prompt(input);
 
-        char **args = malloc(sizeof(char) * 1024);
-        parse(args, input);
+        if (exit_flag != 1) {
 
-        // int i = 0;
-        // while(args[i] != NULL) {
-        //     printf("%s,", args[i]);
-        //     i++;
-        // }
+            char **args = malloc(sizeof(char) * 1024);
+            parse_arguments(args, input);
 
-        if (args[0] == NULL) {
-            printf("Invalid command\n");
-            continue;
+            // Ignores if first argument (command) is empty
+            if (args[0] == NULL) {
+                continue;
+            }
+
+            // Checks for change directory
+            if (strcmp(args[0], "cd") == 0) {
+                if (args[2] == NULL) { // Only two arguments allowed
+                    if (chdir(args[1]) < 0) printf("ERROR: %s\n", strerror(errno));
+                    continue;
+                }
+                printf("Too many arguments!\n");
+            }
+
+            pid_t pid = fork();
+
+            // Child process
+            if (pid == 0) {
+                int rc = execv(args[0], args);
+                if (rc < 0) printf("ERROR: %s\n", strerror(errno));
+            }
+            // Parent process
+            else if (pid > 0) {
+                int status;
+                pid_t child = waitpid(pid, &status, 0);
+                printf("Exit status [%s] = %d\n", args[0], status);
+            }
+
+            free(args);
         }
+        free(input);
 
-        pid_t pid = fork();
-
-        // Child process
-        if (pid == 0) {
-            int rc = execv(args[0], args);
-            if (rc < 0) printf("ERROR\n");
-        }
-        // Parent process
-        else if (pid > 0) {
-            int status;
-            pid_t child = waitpid(pid, &status, 0);
-            printf("Exit status [%s] = %d\n", args[0], status);
-        }
+        if (exit_flag == 1) exit(EXIT_SUCCESS);
     }
 }
